d6d0ca1b83352a76cd82daebeb05f16666def3df
lh7721004 <lh7721004@gmail.com>
2025-06-03 16:32:59 +0900

    feature : 초기 환경 세팅

---
+++ src/test/java/com/example/filesave_back/FilesaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filesave_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
