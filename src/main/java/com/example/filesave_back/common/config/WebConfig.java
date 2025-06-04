17435c3ef97fbb8fad618438ce7b6764d5c29ac5
kimehyun <lh7721004@gmail.com>
2025-06-04 16:37:47 +0900

    update: 배포용 경로로 주소 업데이트

--- src/main/java/com/example/filesave_back/common/config/WebConfig.java
+++ src/main/java/com/example/filesave_back/common/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 요청 허용
-                .allowedOrigins("http://localhost:5173") // 프론트 주소
+                .allowedOrigins("https://filesave.live") // 프론트 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true); // 쿠키/인증정보 포함시 필요
